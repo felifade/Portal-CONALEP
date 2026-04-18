08fc199fcff85f4e760a0c614c2765e1bcfef461
felifade <felifade@gmail.com>
2026-04-18 03:22:27 +0000

    Deploying to gh-pages from @ felifade/Portal-CONALEP@ebc6e9b338ae4ba7a610314274fe4c402d53d906 🚀

--- sw.js
+++ sw.js
@@ -4,7 +4,7 @@
                Network-first para calendar.ics (datos en vivo)
    ============================================================ */
 
-const CACHE_NAME   = 'conalep-portal-v16';
+const CACHE_NAME   = 'conalep-portal-v17';
 const BASE         = '/Portal-CONALEP';
 
 // Recursos a cachear en la instalación
